e1ac23b46d6228af17415d2c5a081243d12d1672
jamijomi344as-jpg <jamijomi344as@gmail.com>
2026-04-24 23:41:12 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Saytning asosiy manziliga kirgan foydalanuvchini 
-  // avtomatik ravishda Login sahifasiga yo'naltiramiz
   redirect("/login");
 }
