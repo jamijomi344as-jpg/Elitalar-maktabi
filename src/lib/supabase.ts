8d062ff1931e60c273a47f6d87928148371beefc
jamijomi344as-jpg <jamijomi344as@gmail.com>
2026-04-26 22:11:03 +0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Browser muhitida xavfsiz bo'lishi uchun tekshiruv
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Supabase kalitlari topilmadi. .env yoki Render muhitini tekshiring.");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
