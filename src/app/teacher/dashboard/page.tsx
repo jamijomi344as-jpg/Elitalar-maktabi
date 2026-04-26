13144724a614f176841afd71acaf90db8d98a5fc
jamijomi344as-jpg <jamijomi344as@gmail.com>
2026-04-26 23:58:23 +0500

    Update page.tsx

--- src/app/teacher/dashboard/page.tsx
+++ src/app/teacher/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { 
   LayoutDashboard, Users, Calendar, Award, Star, BookOpen, 
   Clock, ShieldCheck, Key, CheckCircle, LogOut, Settings, Eye, EyeOff, 
-  TableProperties, Send, AlertCircle, FileText
+  TableProperties, Send, AlertCircle, FileText, X
 } from "lucide-react";
 import { supabase } from "@/lib/supabase";
 
